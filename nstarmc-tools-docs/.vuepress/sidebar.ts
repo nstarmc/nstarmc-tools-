6d6c4be865ef82a2f46bd6d8b3dabe96680e631b
luluxiaoyu <ljy.xiaoyu@qq.com>
2022-04-12 16:00:30 +0800

    1

--- nstarmc-tools-docs/.vuepress/sidebar.ts
+++ nstarmc-tools-docs/.vuepress/sidebar.ts
@@ -2,45 +2,28 @@ import { defineSidebarConfig } from "vuepress-theme-hope";
 
 export default defineSidebarConfig([
   "",
-  "home",
-  "slide",
   {
-    text: "如何使用",
-    icon: "creative",
-    prefix: "guide/",
-    link: "guide/",
+    text: "下载",
+    icon: "icon-xiazai",
+    prefix: "download/",
+    link: "download/",
     children: "structure",
   },
   {
-    text: "文章",
-    icon: "note",
-    prefix: "posts/",
+    text: "帮助文档",
+    icon: "icon-icon_shiyongwendang",
+    prefix: "docs/",
     children: [
-      {
-        text: "文章 1-4",
-        icon: "note",
-        collapsable: true,
-        prefix: "article/",
-        children: ["article1", "article2", "article3", "article4"],
-      },
-      {
-        text: "文章 5-12",
-        icon: "note",
-        children: [
-          {
-            text: "文章 5-8",
-            icon: "note",
-            collapsable: true,
-            prefix: "article/",
-            children: ["article5", "article6", "article7", "article8"],
-          },
-          {
-            text: "文章 9-12",
-            icon: "note",
-            children: ["article9", "article10", "article11", "article12"],
-          },
-        ],
-      },
+      "download_java",
+      "download_pack"
+    ],
+  },
+  {
+    text: "反馈",
+    icon: "icon-fankui",
+    prefix: "feedback/",
+    children: [
+      ""
     ],
   },
 ]);
